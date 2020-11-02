0e22068a212a81b901e462f315aee2d6ece8f965
Frank Hesse <hyperschnell11@outlook.sk>
2020-11-02 07:44:05 -0400

    Update 5.py

--- guia_1/5.py
+++ guia_1/5.py
@@ -1,3 +1,3 @@
 nombre = input("Escriba su nombre\n")
 apellido = input("Escriba su apellido\n")
-print("Su nombre es: ", nombre[::-1], apellido[::-1])
+print("Su nombre es: {} {}".format(nombre[::-1], apellido[::-1]))
